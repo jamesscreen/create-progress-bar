94e4a4b1971717109c320656bfa2f9035bcb155a
paa <andrey.polenok@gmail.com>
2019-01-15 15:16:03 +0300

    простой прогресс бар на javascript

---
+++ script.js
@@ -0,0 +1,17 @@
+function startBar() {
+  var elem = document.getElementById("progressLine");
+  var widthLineBar = 0;
+  
+  var id = setInterval(frame, 10);
+  console.log("id = ", id);
+  
+  function frame(){
+    if(widthLineBar>=100){
+      clearInterval(id);
+    } else {
+      widthLineBar++;
+      elem.style.width = widthLineBar + '%';
+      document.getElementById("label").innerHTML = widthLineBar + '%';
+    }
+  }
+}
